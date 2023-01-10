7c032c434b44a661dfa9dde234a2df979c627050
Inosthful <inosthful@gmail.com>
2023-01-10 16:44:15 +0100

    scrollreveal suite

--- src/js/scrollReveal.js
+++ src/js/scrollReveal.js
@@ -1,34 +1,50 @@
 const sr = ScrollReveal()
  
-sr.reveal('.exo01',{
+
+sr.reveal('.titreSectionPortfolio',{
     delay: 100,
-    duration: 1500,
+    duration: 2000,
+    distance: "65px",
+    origin: "left"
+})
+
+sr.reveal('.iconModal',{
+    delay: 100,
+    duration: 2000,
+    distance: "65px",
+    origin: "left"
+})
+
+sr.reveal('.exo01',{
+    delay: 200,
+    duration: 2000,
     distance: "50px",
     origin: "top"
 });
 
 sr.reveal('.exo02',{
     delay: 400,
-    duration: 1500,
+    duration: 2000,
     distance: "50px",
     origin: "top",
 
 });
 sr.reveal('.exo03',{
-    delay: 700,
-    duration: 1500,
+    delay: 600,
+    duration: 2000,
     distance: "50px",
     origin: "top"
 });
 sr.reveal('.exo04',{
-    delay: 1000,
-    duration: 1500,
+    delay: 800,
+    duration: 2000,
     distance: "50px",
     origin: "top"
 });
 sr.reveal('.exo05',{
-    delay: 1300,
-    duration: 1500,
+    delay: 1000,
+    duration: 2000,
     distance: "50px",
     origin: "top"
 });
+
