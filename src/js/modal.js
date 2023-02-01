a7ad0b9b613b81fd34b4ca8ae37f254f18a6eac1
Inosthful <inosthful@gmail.com>
2023-02-01 15:48:25 +0100

    hover on preview bien commencé, voir titre de la section (suite)

--- src/js/modal.js
+++ src/js/modal.js
@@ -125,3 +125,10 @@
 //   }
 // })
 
+
+let p1 = document.getElementById('p1');
+let p2 = document.getElementById('p2');
+$(function(){
+    $('#p1 a').miniPreview({ prefetch: 'pageload' });
+    $('#p2 a').miniPreview({ prefetch: 'pageload' });
+})
