8ac3b65b7d2f7f08047a1a48d2c599402ce34587
Inosthful <inosthful@gmail.com>
2023-02-22 10:48:02 +0100

    animation loader

--- src/js/modal.js
+++ src/js/modal.js
@@ -129,6 +129,8 @@
 let p1 = document.getElementById('p1');
 let p2 = document.getElementById('p2');
 $(function(){
-    $('#p1 a').miniPreview({ prefetch: 'pageload' });
+    $('#p1 a').miniPreview({ 
+        prefetch: 'pageload'
+});
     $('#p2 a').miniPreview({ prefetch: 'pageload' });
 })
